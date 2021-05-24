5570a5a954403e80d7944b5dc316a107dc583d02
andressbolivarr <81452502+andressbolivarr@users.noreply.github.com>
2021-05-24 19:15:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById('burger')
+const navbar = document.getElementById('navbar')
+
+	burger.addEventListener('click', () =>{
+		navbar.classList.toggle('see');
+	});
